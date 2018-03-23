529def61101ca1d6a28247f452871493b6170d30
Andreas Boss <andi@theboss.ch>
2018-03-23 20:01:40 +0100

    Select, Deleting, Query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,10 @@ interface Post {
   content: string;
 }
 
+interface PostId extends Post {
+  id: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,22 +20,42 @@ interface Post {
 
 export class AppComponent {
   postsCollection: AngularFirestoreCollection<Post>;
-  posts: Observable<Post[]>;
+  posts: any;
 
   title: string;
   content: string;
+
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
 
   constructor(private afs: AngularFirestore) {
 
   }
 
   ngOnInit() {
-    this.postsCollection = this.afs.collection('posts');
-    this.posts = this.postsCollection.valueChanges();
+    this.postsCollection = this.afs.collection('posts', ref => ref.where('title', '==', 'tafli'));
+    this.posts = this.postsCollection.snapshotChanges()
+      .map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;
+
+          return {id, data};
+        });
+      });
   }
 
   addPost() {
-    // this.afs.collection('posts').add({'title': this.title, 'content': this.content});
-    this.afs.collection('posts').doc('my-custom-id').set({'title': this.title, 'content': this.content});
+    this.afs.collection('posts').add({'title': this.title, 'content': this.content});
+    // this.afs.collection('posts').doc('my-custom-id').set({'title': this.title, 'content': this.content});
+  }
+
+  getPost(postId) {
+    this.postDoc = this.afs.doc('posts/' + postId);
+    this.post = this.postDoc.valueChanges();
+  }
+
+  deletePost(postId) {
+    this.afs.doc('posts/'+postId).delete();
   }
 }
