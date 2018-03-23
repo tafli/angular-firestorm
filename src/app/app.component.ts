60844a582cb820c41d5e0033c86c73d9bf7f5d16
Andreas Boss <andi@theboss.ch>
2018-03-23 19:43:41 +0100

    Working!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Post {
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  postsCollection: AngularFirestoreCollection<Post>;
+  posts: Observable<Post[]>;
+
+  title: string;
+  content: string;
+
+  constructor(private afs: AngularFirestore) {
+
+  }
+
+  ngOnInit() {
+    this.postsCollection = this.afs.collection('posts');
+    this.posts = this.postsCollection.valueChanges();
+  }
+
+  addPost() {
+    // this.afs.collection('posts').add({'title': this.title, 'content': this.content});
+    this.afs.collection('posts').doc('my-custom-id').set({'title': this.title, 'content': this.content});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
 
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyDO6QQiypQJdaI2jFy1pMYgqFNl7AYU8nA',
+  authDomain: 'angular-firebase-f577b.firebaseapp.com',
+  databaseURL: 'https://angular-firebase-f577b.firebaseio.com',
+  projectId: 'angular-firebase-f577b',
+  storageBucket: 'angular-firebase-f577b.appspot.com',
+  messagingSenderId: '955344340638'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
